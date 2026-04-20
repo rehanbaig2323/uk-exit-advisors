da9a6ef20dde348b73813f62baa0f9b198a1cb47
Rehan <rb2323@ic.ac.uk>
2026-04-20 22:19:13 +0000

    Section 12: add global advisor search

---
+++ src/data/search-index.ts
@@ -0,0 +1,8 @@
+import { advisorsData } from '@/data/advisors';
+
+export const searchIndex = advisorsData.map((advisor) => ({
+  slug: advisor.slug,
+  name: advisor.name,
+  location: advisor.location,
+  terms: [advisor.name, advisor.location, ...advisor.services, ...advisor.exitRoutes, ...advisor.advisorTypes].join(' ').toLowerCase(),
+}));
